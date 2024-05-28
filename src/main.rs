e6e14f4eaf891f2a915dcaee98904d146c8a35d1
marco <toyko2001@gmail.com>
2024-05-28 15:07:15 +0630

    fix(handle): fix hello_world handler

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+  response::{Html, IntoResponse}, routing::get, Router
+};
+
+async fn handle_hello() -> impl IntoResponse {
+  println!("->> {:<12} - handle_hello", "HANDLER");
+
+  Html("Hello <strong>World!!</strong>")
+}
 
 #[tokio::main]
 async fn main() {
-  let routes_hello = Router::new().route(
-    "/hello",
-    get(|| async { Html("Hello <strong>World!!</strong>") }),
-  );
+  let routes_hello = Router::new().route("/hello", get(handle_hello));
 
   // region:      ───── Start Server
   let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
