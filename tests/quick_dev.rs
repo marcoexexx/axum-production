3f1041928952a820885c9784a3be0005dbe6c168
marco <toyko2001@gmail.com>
2024-06-12 13:29:18 +0630

    add test BASELINE

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,8 +1,20 @@
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
   let hc = httpc_test::new_client("http://localhost:8000")?;
+
+  hc.do_get("/index.html").await?.print().await?;
+
+  let req_login = hc.do_post(
+    "/api/login",
+    json!({
+      "username": "demo1",
+      "password": "welcome",
+    }),
+  );
+  req_login.await?.print().await?;
 
   Ok(())
 }
