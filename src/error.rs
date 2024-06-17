afa915b766339a0bbb663d325759020c16180158
marco <toyko2001@gmail.com>
2024-06-17 11:49:01 +0630

    setup axum server

---
+++ src/error.rs
@@ -0,0 +1,14 @@
+use std::fmt::Display;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {}
+
+impl Display for Error {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{self:?}")
+  }
+}
+
+impl std::error::Error for Error {}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use axum::Router;
+pub use error::Result;
+
+use tokio::{net::TcpListener, signal};
+
+mod error;
+
+async fn shutdown_signal() {
+  signal::ctrl_c().await.unwrap();
+}
+
 #[tokio::main]
-async fn main() {
-  println!("Hello world");
+async fn main() -> Result<()> {
+  let routes = Router::new();
+
+  let tcp_listener = TcpListener::bind("localhost:8000").await.unwrap();
+  println!("🚀 Server is ready http://localhost:8000");
+
+  axum::serve(tcp_listener, routes.into_make_service())
+    .with_graceful_shutdown(shutdown_signal())
+    .await
+    .unwrap();
+
+  Ok(())
 }
