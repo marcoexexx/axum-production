a5568effa64d087933d01c4edc7e43cd7e0b895f
marco <toyko2001@gmail.com>
2024-06-17 11:35:39 +0630

    restart all code

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,4 @@
-use axum::{middleware, Router};
-use model::ModelManager;
-use tokio::net::TcpListener;
-
-use error::Result;
-use tower_cookies::CookieManagerLayer;
-use tracing_subscriber::EnvFilter;
-
-mod ctx;
-mod error;
-mod log;
-mod model;
-mod web;
-
 #[tokio::main]
-async fn main() -> Result<()> {
-  tracing_subscriber::fmt()
-    .without_time()
-    .with_target(false)
-    .with_env_filter(EnvFilter::from_default_env())
-    .init();
-
-  let mm = ModelManager::new().await?;
-
-  let routes = Router::new()
-    .merge(web::routes_login::routes())
-    .layer(middleware::map_response(
-      web::mw_response_map::mw_response_map,
-    ))
-    .layer(middleware::from_fn_with_state(
-      mm.clone(),
-      web::mw_auth::mw_ctx_resolver,
-    ))
-    .layer(CookieManagerLayer::new())
-    .fallback_service(web::routes_static::serve_dir());
-
-  let tcp_listener = TcpListener::bind("localhost:8000").await.unwrap();
-
-  axum::serve(tcp_listener, routes.into_make_service())
-    .await
-    .unwrap();
-
-  Ok(())
+async fn main() {
+  println!("Hello, world");
 }
--- src/web/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod error;
-pub mod mw_auth;
-pub mod mw_response_map;
-pub mod routes_login;
-pub mod routes_static;
-
-pub const AUTH_TOKEN: &str = "auth-token";
